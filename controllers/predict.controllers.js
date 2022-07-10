4557e3986233aae53a1ff8c4e8ae85db9848d04e
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-10 22:43:06 +0500

    Initial commit

---
+++ controllers/predict.controllers.js
@@ -0,0 +1,47 @@
+const tf = require('@tensorflow/tfjs');
+const mobilenet = require('@tensorflow-models/mobilenet');
+const image = require('get-image-data');
+const fs = require('fs');
+//loading
+exports.loadmodel = async function MobilenetLoad(){
+   console.log('Loading.......')
+   const model = await mobilenet.load();
+};
+
+exports.makePredictions = async (req, res, next) => {
+  const imagePath = './images/test-image.jpg';
+  try {
+    const loadModel = async (img) => {
+      const output = {};
+      // classify
+      console.log("Predicting...");
+      output.predictions = await model.classify(img);
+      console.log(output);
+      res.statusCode = 200;
+      res.json(output);
+    };
+    await image(imagePath, async (err, imageData) => {
+      // pre-process image
+      const numChannels = 3;
+      const numPixels = imageData.width * imageData.height;
+      const values = new Int32Array(numPixels * numChannels);
+      const pixels = imageData.data;
+      for (let i = 0; i < numPixels; i++) {
+        for (let channel = 0; channel < numChannels; ++channel) {
+          values[i * numChannels + channel] = pixels[i * 4 + channel];
+        }
+      }
+      const outShape = [imageData.height, imageData.width, numChannels];
+      const input = tf.tensor3d(values, outShape, 'int32');
+      await loadModel(input);
+// delete image file
+      fs.unlinkSync(imagePath, (error) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
